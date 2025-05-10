71623bc246eac6df8ea47c3f266f43c7a198043b
batukarabulut <batuukarabulut@gmail.com>
2025-05-11 00:04:46 +0300

    Refactor movie rendering and loading UI; implement error handling and loading indicators

--- Film Finder/api.js
+++ Film Finder/api.js
@@ -1,3 +1,5 @@
+import { renderMovies, showError, showLoading, hideLoading } from './ui.js';
+
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
 
@@ -5,13 +7,26 @@ const getMovies = async () => {
     const searchValue = searchInput.value.trim();
     if (!searchValue) return;
 
+    showLoading();
+
     try {
         const response = await fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=5eefd4fc`);
         const data = await response.json();
+
+        if(data.Response === "True") {
+            renderMovies(data.Search);
+        }
+        else{
+            showError(data.Error || "Cannot find movie!");
+        }
+
         console.log(data);
     }
     catch (error) {
         console.error("Cannot find movie data.", error);
+    }
+    finally {
+        hideLoading
     }
 
 }
--- Film Finder/ui.js
+++ Film Finder/ui.js
@@ -1,6 +1,95 @@
-const renderMovies = (movies) => {
-    const resultsContainer = document.getElementById("results");
+const resultsContainer = document.getElementById("results");
+
+export const renderMovies = (movies) => {
     resultsContainer.innerHTML = "";
+
+    if(movies.length === 0){
+        return;
+    }
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("movie-card");
+
+        const moviePoster = document.createElement("div");
+        moviePoster.classList.add("movie-poster"); // will append on movie-card
+
+        const posterImg = document.createElement("img");
+        posterImg.src = movie.Poster === "N/A" 
+        ? "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png" : movie.Poster;
+        posterImg.alt = movie.Title;
+
+        moviePoster.appendChild(posterImg);
+
+        const movieInfo = document.createElement("div");
+        movieInfo.classList.add("movie-info"); // will apend on movie-card
+
+        const movieTitle = document.createElement("h2");
+        movieTitle.classList.add("movie-title");
+        movieTitle.textContent = movie.Title;
+
+        const movieYear = document.createElement("p");
+        movieYear.classList.add("movie-year");
+        movieYear.textContent = `Year: ${movie.Year}`;
+
+        const movieType = document.createElement("p");
+        movieType.classList.add("movie-type");
+        movieType.textContent = `Type: ${movie.Type}`;
+
+        const movieId = document.createElement("p");
+        movieId.classList.add("movie-id");
+        movieId.textContent = `IMDB Id: ${movie.imdbID}`;
+    
+        movieInfo.appendChild(movieTitle);
+        movieInfo.appendChild(movieYear);
+        movieInfo.appendChild(movieType);
+        movieInfo.appendChild(movieId);
+
+        movieCard.appendChild(moviePoster);
+        movieCard.appendChild(movieInfo);
+
+        resultsContainer.appendChild(movieCard);        
+    })
+};
+
+export const showError = (message) => {
+    resultsContainer.innerHTML = "";
+    resultsContainer.textContent = `${message}`;
+
+    while(resultsContainer.firstChild){
+        resultsContainer.removeChild(resultsContainer.firstChild);
+    }
+
+    const errorCard = document.createElement("div");
+    errorCard.classList.add("error-card");
+    
+    const errorText = document.createElement("p");
+    errorText.textContent = message || "Movies cannot found. Please try another movie.";
+
+    errorCard.appendChild(errorText);
+
+    resultsContainer.appendChild(errorCard);
+};
+
+export const showLoading = () => {
+    resultsContainer.innerHTML = "";
+
+    const loadingDiv = document.createElement("div");
+    loadingDiv.classList.add("loading");
+
+    const loadingText = document.createElement("p");
+    loadingText.textContent = "Loading movies...";
+
+    const loadingSpinner = document.createElement("div");
+    loadingSpinner.classList.add("loading-spinner");
+
+    loadingDiv.appendChild(loadingText);
+    loadingDiv.appendChild(loadingSpinner);
     
 
-}
+    resultsContainer.appendChild(loadingDiv);
+};
+
+export const hideLoading = () => {
+
+};
